41b7f75e6110aa2c8d99332dd7413ca7696e1684
Nukeador <nukeador@gmail.com>
2017-08-22 16:47:46 +0200

    Deprecated code into standards

--- src/background.js
+++ src/background.js
@@ -3,46 +3,46 @@ var settings = new Settings();
 settings.listen('GlobalSettings');
 
 if (settings.retrieve('hideIcon')) {
-	chrome.pageAction.hide();
+	browser.pageAction.hide();
 }
 
 var iconActionType = 'pageAction';
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.controller != 'Background_HTML') return;
 	try {
 		if (request.action == "setPwdHashIconOn") {
 			if (!settings.retrieve('hideIcon')) {
-				//chrome.tabs.getSelected(null, function(tab) {
-					chrome[iconActionType].setIcon({
+				//browser.tabs.getSelected(null, function(tab) {
+					browser.pageAction.setIcon({
 						tabId: sender.tab.id,
 						path: "images/icon19on.png"
 					});
 				//});
 			}
 			sendResponse({ok: true});
-			
+
 		} else if (request.action == "setPwdHashIconOff") {
 			if (!settings.retrieve('hideIcon')) {
-				//chrome.tabs.getSelected(null, function(tab) {
-					chrome[iconActionType].setIcon({
+				//browser.tabs.getSelected(null, function(tab) {
+					browser.pageAction.setIcon({
 						tabId: sender.tab.id,
 						path: "images/icon19off.png"
 					});
 				//});
 			}
 			sendResponse({ok: true});
-			
+
 		} else if (request.action == "showPwdHashIcon") {
 			if (!settings.retrieve('hideIcon')) {
-				//chrome.tabs.getSelected(null, function(tab) {
-					chrome[iconActionType].show(sender.tab.id);
+				//browser.tabs.getSelected(null, function(tab) {
+					browser.pageAction.show(sender.tab.id);
 				//});
 			}
 			sendResponse({ok: true});
 		}
 
-		
+
 	} catch (e) {
 		sendResponse({ok: false});
 		console.log(e);
